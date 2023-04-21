15c461bbd95f1287b9aff6ff202157b199f2e9f6
Hélder Costa <helderdacsta@hotmail.com>
2023-04-21 09:28:17 +0100

    Actualização

---
+++ conta-banco/src/edu/helder/edu/helder/ContaTerminal.java
@@ -0,0 +1,29 @@
+package edu.helder;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Por favor digite  o número da agencia!");
+      String agencia = sc.nextLine();
+
+      System.out.println("Por favor digite  o nome do cliente!");
+      String nomeCliente = sc.nextLine();
+      
+      System.out.println("Por favor digite  o número da conta!");
+      int numeroConta = sc.nextInt();
+
+      System.out.println("Por favor digite  o valor a depositar!");
+      Double saldo = sc.nextDouble();
+
+      System.out.print("Olá "+ nomeCliente);
+      System.out.print(", obrigado por criar uma conta no nosso banco, a sua Agencia é "+ agencia );
+      System.out.print(",conta bancaria número "+ numeroConta );
+      System.out.println(" e seu saldo é "+ saldo + " já esta dispónivel para saque.");
+
+      sc.close();
+ 
+
+    }
+}
