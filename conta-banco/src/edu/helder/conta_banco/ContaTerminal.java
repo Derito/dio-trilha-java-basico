fbdb0a3e538909b7479a5d34e8a9a6cd92df5c0b
Hélder Costa <helderdacsta@hotmail.com>
2023-04-24 14:54:49 +0100

    Criação da pasta exercícios

--- conta-banco/src/edu/helder/conta_banco/ContaTerminal.java
+++ conta-banco/src/edu/helder/conta_banco/ContaTerminal.java
@@ -2,6 +2,7 @@ package conta_banco;
 
 import java.util.Scanner;
 
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
       Scanner sc = new Scanner(System.in);
---
+++ conta-banco/src/edu/helder/exercicios/LacoFor.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class LacoFor {
+    
+    public static void main(String[] args) {
+        String alunos [] = {"Hélder", "João","Paulo","Mateus","Luis"};
+
+        for(String aluno : alunos){
+            System.out.println(" O nome do aluno é: " +  aluno);
+        }
+    }
+}
